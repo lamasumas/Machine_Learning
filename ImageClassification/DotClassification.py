5f4ff8f96919942c7ae7121cdbefcfb98d582bcd
els <navesllanes@gmail.com>
2019-07-31 16:42:38 +0100

    project done, I will try to add another kernel

--- ImageClassification/DotClassification.py
+++ ImageClassification/DotClassification.py
@@ -13,18 +13,30 @@ label_test = np.loadtxt("myLabel_test.txt")
 
 data_test_individual = np.loadtxt("onlyOne.txt")
 label_test_individual = int(np.loadtxt("onlyOneLabel.txt"))
-
+"""
+plt.figure(figsize=(8,8))
+for k in range(0,100):
+    plt.subplot(10,10,k+1)
+    ## Each row is a character
+    imagen = data_train[k, ]
+    ## Each character is 28x28 pixels
+    imagen = imagen.reshape(28, 28)
+    plt.imshow(imagen, cmap='gray')
+    plt.axis('off')
+plt.show()
+"""
 C = 1
 n = data_train.shape[0]
 m = data_test.shape[0]
 I = np.identity(n)
+
 a = 1
 b = 3
-number_of_posibilities = 17
+number_of_posibilities = 5
 
-H = data_train / 255
-H_test = data_test / 255
-H_individual = data_test_individual / 255
+H = data_train // 255
+H_test = data_test // 255
+H_individual = data_test_individual // 255
 
 labels = np.zeros((n, number_of_posibilities))
 
@@ -44,8 +56,31 @@ labels_from_test = np.dot(Omega_test, W)
 predicted_class = labels_from_test.argmax(axis=1)
 accuracy_percentage = np.sum(predicted_class == label_test)/float(m)*100.
 print ('Accuracy = %.1f%%' % accuracy_percentage)
+"""
+from sklearn.metrics import confusion_matrix
 
+mc = confusion_matrix(label_test, predicted_class)
 
+print (u'Matriz de confusión')
+print (mc)
+
+plt.figure(figsize=(10,10))
+ticks = range(17)
+plt.xticks(ticks)
+plt.yticks(ticks)
+plt.imshow(mc,cmap="Oranges_r",interpolation='nearest')
+plt.colorbar(shrink=0.8)
+w, h = mc.shape
+for i in range(w):
+    for j in range(h):
+        plt.annotate(str(mc[i][j]), xy=(j, i), 
+                    horizontalalignment='center',
+                    verticalalignment='center')
+plt.xlabel('Etiqueta predicha')
+plt.ylabel('Etiqueta')
+plt.title(u'Matriz de Confusión')
+plt.show()
+"""
 Omega_test_individual = kernel_polinomial(H_individual, H)
 labels_from_test_individual = np.dot(Omega_test_individual, W)
 
--- ImageClassification/ImageWidthDotGeneration.py
+++ ImageClassification/ImageWidthDotGeneration.py
@@ -4,16 +4,38 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import random
 
-def generateImage():
+number_of_possible_groups = [0,1,2,3,4]
+
+def generateImage(number_of_groups):
     futureImageArray = np.zeros((28,28))
-    number_of_groups = 0
-    for i in range(4):
-        for j in range(4):
-            if random.randint(0,2) == 1:
-                futureImageArray[(i*7)-1:((i-1)*7)+1:-1,(j*7)-1:((j-1)*7)+1:-1] = 255
-                number_of_groups += 1
+    counter_iterations = 0
+    counter_groups_written = 0
+    for i in range(2):
+        for j in range(2):
+            if number_of_groups - counter_groups_written == 0:
+                break
+            elif 4 - counter_iterations <= number_of_groups - counter_groups_written :
+                futureImageArray[(i*14)-1:((i-1)*14)+1:-1,(j*14)-1:((j-1)*14)+1:-1] = 255
+                counter_groups_written += 1
+            else:
+                if random.randint(0,2) == 1:
+                    futureImageArray[(i*14)-1:((i-1)*14)+1:-1,(j*14)-1:((j-1)*14)+1:-1] = 255
+                    counter_groups_written += 1
+
+            counter_iterations += 1
 
     return futureImageArray, number_of_groups
+
+def generateImageTest():
+    futureImageArray = np.zeros((28,28))
+    number_of_groups = 0
+    for i in range(2):
+        for j in range(2):
+            if random.randint(0,2) == 1:
+                futureImageArray[(i*14)-1:((i-1)*14)+1:-1,(j*14)-1:((j-1)*14)+1:-1] = 255
+                number_of_groups += 1
+    return futureImageArray, number_of_groups
+
 
 
 def saveImagePng(thePicture):
@@ -31,8 +53,13 @@ def saveImage(allPictures, theArrayToSave):
 def generateTrainFiles():
     allPictures = []
     allLabels = []
-    for i in range(900):
-        currentPicture, currentLabel = generateImage()
+    copy_possible_groups = number_of_possible_groups.copy()
+    for i in range(16000):
+        group_number_selected = copy_possible_groups[random.randint(0, (len(copy_possible_groups)-1))]
+        copy_possible_groups.remove(group_number_selected)
+        if len(copy_possible_groups) == 0:
+            copy_possible_groups = number_of_possible_groups.copy()
+        currentPicture, currentLabel = generateImage(group_number_selected)
         saveImage(allPictures,currentPicture)
         allLabels.append(currentLabel)
     
@@ -50,8 +77,7 @@ def generateTrainFiles():
 
 def generateOnePictureAndLabel():
     thePicture = []
-   
-    currentPicture, currentLabel = generateImage()
+    currentPicture, currentLabel = generateImageTest()
     saveImage(thePicture,currentPicture)
     saveImagePng(currentPicture)
     dataToSave = open("onlyOne.txt", "a")
@@ -70,7 +96,7 @@ def generateTestFiles():
     allPictures = []
     allLabels = []
     for i in range(100):
-        currentPicture, currentLabel = generateImage()
+        currentPicture, currentLabel = generateImageTest()
         saveImage(allPictures,currentPicture)
         allLabels.append(currentLabel)
     
@@ -88,3 +114,5 @@ def generateTestFiles():
    
 if __name__ =="__main__":
     generateOnePictureAndLabel()
+    generateTestFiles()
+    generateTrainFiles()
