752954294c06ba065ba597580b47324f379eba65
els <navesllanes@gmail.com>
2019-07-31 13:14:03 +0100

    Done

--- ImageClassification/DotClassification.py
+++ ImageClassification/DotClassification.py
@@ -11,6 +11,9 @@ label_train = np.loadtxt("myLabel_train.txt")
 data_test = np.loadtxt("myData_test.txt")
 label_test = np.loadtxt("myLabel_test.txt")
 
+data_test_individual = np.loadtxt("onlyOne.txt")
+label_test_individual = int(np.loadtxt("onlyOneLabel.txt"))
+
 C = 1
 n = data_train.shape[0]
 m = data_test.shape[0]
@@ -21,6 +24,7 @@ number_of_posibilities = 17
 
 H = data_train / 255
 H_test = data_test / 255
+H_individual = data_test_individual / 255
 
 labels = np.zeros((n, number_of_posibilities))
 
@@ -40,3 +44,10 @@ labels_from_test = np.dot(Omega_test, W)
 predicted_class = labels_from_test.argmax(axis=1)
 accuracy_percentage = np.sum(predicted_class == label_test)/float(m)*100.
 print ('Accuracy = %.1f%%' % accuracy_percentage)
+
+
+Omega_test_individual = kernel_polinomial(H_individual, H)
+labels_from_test_individual = np.dot(Omega_test_individual, W)
+
+print("The program thinks that there are " + str(labels_from_test_individual.argmax()))
+print("Actually the real answer is " + str(label_test_individual))
